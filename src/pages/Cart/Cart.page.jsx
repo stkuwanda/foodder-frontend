283bac72ff3d0d97194d8a2447eaec38c0bba8b4
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-04-15 22:45:56 +0200

    implementing styling and item removing feature on Cart page

--- src/pages/Cart/Cart.page.jsx
+++ src/pages/Cart/Cart.page.jsx
@@ -2,32 +2,40 @@ import { useStoreContext } from '../../context/StoreContext/StoreContext.tools';
 import './Cart.page.css';
 
 function Cart() {
-  const { cartItems, food_list, removeFromCart} = useStoreContext();
+	const { cartItems, food_list, removeFromCart } = useStoreContext();
 
-  return (
-    <main className='cart'>
-      <section className="cart-items">
-        <div className="cart-items-title">
-          <p>Items</p>
-          <p>Title</p>
-          <p>Price</p>
-          <p>Quantity</p>
-          <p>Total</p>
-          <p>Remove</p>
-        </div>
-        <br />
-        <hr />
-        {food_list.map(item => (cartItems[item._id] && (<div className='cart-items-title cart-item'>
-          <img src={item.image} alt={`Image of ${item.name}`} />
-          <p>{item.name}</p>
-          <p>{item.price}</p>
-          <p>{cartItems[item._id]}</p>
-          <p>{item.price * cartItems[item._id]}</p>
-          <p>x</p>
-        </div>)))}
-      </section>
-    </main>
-  )
+	return (
+		<main className='cart'>
+			<section className='cart-items'>
+				<div className='cart-items-title'>
+					<p>Items</p>
+					<p>Title</p>
+					<p>Price</p>
+					<p>Quantity</p>
+					<p>Total</p>
+					<p className='cross'>Remove</p>
+				</div>
+				<br />
+				<hr />
+				{food_list.map(
+					(item) =>
+						cartItems[item._id] > 0 && (
+							<div>
+								<div className='cart-items-title cart-item'>
+									<img src={item.image} alt={`Image of ${item.name}`} />
+									<p>{item.name}</p>
+									<p>${item.price}</p>
+									<p>{cartItems[item._id]}</p>
+									<p>${item.price * cartItems[item._id]}</p>
+									<p className='cross' onClick={() => removeFromCart(item._id)}>x</p>
+								</div>
+								<hr />
+							</div>
+						)
+				)}
+			</section>
+		</main>
+	);
 }
 
 export default Cart;
