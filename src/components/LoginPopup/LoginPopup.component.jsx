f627c693e2577e0140c62b1197228ed0a042e79e
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-04-20 11:56:53 +0200

    updating LoginPopup with basic state to support user login and registration

--- src/components/LoginPopup/LoginPopup.component.jsx
+++ src/components/LoginPopup/LoginPopup.component.jsx
@@ -3,9 +3,21 @@ import { assets } from '../../assets/assets';
 import './LoginPopup.component.css';
 
 function LoginPopup({ setShowLogin }) {
-  const [currState, setCurrentState] = useState('Login');
+	const [currState, setCurrentState] = useState('Login');
+	const [userData, setUserData] = useState({
+		name: '',
+		email: '',
+		password: '',
+	});
 
-  return (
+	function onChangeHandler(event) {
+		const name = event.target.name;
+		const value = event.target.value;
+
+		setUserData((prev) => ({ ...prev, [name]: value }));
+	}
+
+	return (
 		<div className='login-popup'>
 			<form className='login-popup-container'>
 				<div className='login-popup-title'>
@@ -17,16 +29,49 @@ function LoginPopup({ setShowLogin }) {
 					/>
 				</div>
 				<div className='login-popup-inputs'>
-          {currState === 'Login' ? undefined : (<input type='text' placeholder='Enter your full name' required />)}
-          <input type="email" placeholder='Enter your email' required />
-          <input type="password" placeholder='Password' required />
+					{currState === 'Login' ? undefined : (
+						<input
+							type='text'
+							name='name'
+							onChange={onChangeHandler}
+							value={userData.name}
+							placeholder='Enter your full name'
+							required
+						/>
+					)}
+					<input
+						type='email'
+						name='email'
+						placeholder='Enter your email'
+						onChange={onChangeHandler}
+						value={userData.email}
+						required
+					/>
+					<input
+						type='password'
+						name='password'
+						onChange={onChangeHandler}
+						value={userData.password}
+						placeholder='Password'
+						required
+					/>
 				</div>
-        <button>{currState === 'Sign Up' ? 'Sign Up' : 'Login' }</button>
-        <div className="login-popup-condition">
-          <input type="checkbox" required />
-          <p>By continuing, I agree to the terms of use and privacy policy</p>
-        </div>
-        {currState === 'Login' ? <p>Do you want to sign up? <span onClick={() => setCurrentState('Sign Up')}>Click here</span></p> : <p>Already signed up? <span onClick={() => setCurrentState('Login')}>Login</span></p>}
+				<button>{currState === 'Sign Up' ? 'Sign Up' : 'Login'}</button>
+				<div className='login-popup-condition'>
+					<input type='checkbox' required />
+					<p>By continuing, I agree to the terms of use and privacy policy</p>
+				</div>
+				{currState === 'Login' ? (
+					<p>
+						Do you want to sign up?{' '}
+						<span onClick={() => setCurrentState('Sign Up')}>Click here</span>
+					</p>
+				) : (
+					<p>
+						Already signed up?{' '}
+						<span onClick={() => setCurrentState('Login')}>Login</span>
+					</p>
+				)}
 			</form>
 		</div>
 	);
